ee11e6a3cb91d0acee0353c30dec26697e03ed0d
sree296 <nivas296@gmail.com>
2022-09-22 11:37:25 +0530

    Fluent Validator Changes

--- c#/NZWalks/NZWalks.API/Program.cs
+++ c#/NZWalks/NZWalks.API/Program.cs
@@ -1,3 +1,4 @@
+using FluentValidation.AspNetCore;
 using Microsoft.EntityFrameworkCore;
 using NZWalks.API.Data;
 using NZWalks.API.Repositories;
@@ -10,6 +11,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddFluentValidation(options => options.RegisterValidatorsFromAssemblyContaining<Program>());
 
 // Injecting DbContext
 builder.Services.AddDbContext<NZWalksDbContext>(options =>
@@ -20,7 +22,6 @@ builder.Services.AddDbContext<NZWalksDbContext>(options =>
 builder.Services.AddScoped<IRegionRepository, RegionRepository>();
 builder.Services.AddScoped<IWalkRepository, WalkRepository>();
 builder.Services.AddScoped<IWalkDifficultyRespository, WalkDifficultyRespository>();
-
 builder.Services.AddAutoMapper(typeof(Program).Assembly);
 
 var app = builder.Build();
---
+++ c#/NZWalks/NZWalks.API/Validators/WalkDifficultyRequestValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using System.Runtime.CompilerServices;
+
+namespace NZWalks.API.Validators
+{
+    public class WalkDifficultyRequestValidator: AbstractValidator<Models.DTO.WalkDifficultyRequest>
+    {
+        public WalkDifficultyRequestValidator()
+        {
+            RuleFor(x => x.Code).NotEmpty();
+        }
+    }
+}
